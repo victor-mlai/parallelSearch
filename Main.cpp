5c3c72f7f6f6b257e37d633234b04733e6d074af
Vladut Buhoci <bhc_vladut@yahoo.com>
2019-11-17 20:08:27 +0200

    templetized and refactorized

--- Main.cpp
+++ Main.cpp
@@ -3,67 +3,81 @@
 #include <vector>
 #include <omp.h>
 #include <time.h>
+#include <iostream>
+#include <chrono>
 
-#define N 1500
+constexpr unsigned long long N = 1'299'000'000;
 
 /*
-val = 8
+val = 7
                      P1               P2         
 arr = 0  1  2  3  4  5  6  7  8  9 10 11 12 13 14
-c = 1                ?                ?           -1
+c = right            ?                ?           left
 
 after the first while:
 					 P1               P2
 arr = 0  1  2  3  4  5  6  7  8  9 10 11 12 13 14
-c = 1                1                -1          -1
+c = right            right            left        left
                      ^                ^
    					these 2 are different
-
+thrdPos[P1] = 6
+thrdPos[P2] = 11
 => l = 6, r = 10;
 
-		     P1    P2 
-arr = ... 6  7  8  9  10...
+	     P1       P2 
+arr = 6  7  8  9  10 11
 
-=> P1 will find the value at index 4
+=> P1 will find the value at index 7
 */
-int parallelSearch(std::vector<int> arr, int val) {
-	int l = 0;	// left most index
-	int r = N - 1; // right most index
+template<typename VecType, typename VecSizeT = unsigned long long>
+VecSizeT parallelSearch(const std::vector<VecType>& arr, VecType val)
+{
+	VecSizeT l = 0;	// left most index
+	VecSizeT r = N - 1; // right most index
 
 	enum state { left = -1, right = 1 };
-	int p = omp_get_max_threads();
-	std::vector<state> c(p + 2);
+	VecSizeT threadCount = omp_get_max_threads();
 
-	c[0] = right;
-	c[p+1] = left;
+	// each thread will tell if "val" is to their "left" or to their "right"
+	std::vector<state> dirToVal(threadCount + 2); // + 2 <=> the 2 borders
 
-	int g = (int)ceil(log2(N + 1) / log2(p + 1));	// nr pasi <=> complexity O(g)
+	dirToVal[0] = right;	// left border 
+	dirToVal[threadCount+1] = left;
 
-	std::vector<int> j(p);	// position for each process
+	//int g = (int)ceil(log2(N + 1) / log2(p + 1));	// nr pasi <=> complexity O(g)
 
-	int index = -1;
-	int temp_index = -1;
+	std::vector<VecSizeT> thrdPos(threadCount);	// position for each thread
 
-	#pragma omp parallel shared(l, r, p, c, g, j, index, temp_index)
+	VecSizeT index = -1;
+	VecSizeT temp_index = -1;
+
+	#pragma omp parallel shared(l, r, threadCount, index, temp_index)
 	{
-		int proc_id = omp_get_thread_num();
+		VecSizeT thrdID = omp_get_thread_num();
+		//printf("Number of threads: %d\n", omp_get_max_threads());
 
-		while (l <= r && index == -1) {
-			int segm_size = (r - l + 1) / (p + 1) + 1;
-			j[proc_id] = l + (proc_id + 1) * segm_size - 1;
+		while (l <= r && index == -1)
+		{
+			VecSizeT segm_size = (r - l + 1) / (threadCount + 1) + 1;
+			thrdPos[thrdID] = l + (thrdID + 1) * segm_size - 1;
 
-			if (j[proc_id] > r) {
-				c[proc_id + 1] = left;
+			if (thrdPos[thrdID] > r)
+			{
+				dirToVal[thrdID + 1] = left;
 			}
-			else {
-				if (arr[j[proc_id]] == val) {
-					temp_index = j[proc_id];
+			else
+			{
+				if (arr[thrdPos[thrdID]] == val)
+				{
+					temp_index = thrdPos[thrdID];
 				}
-				else if (arr[j[proc_id]] < val) {
-					c[proc_id + 1] = right;
+				else if (arr[thrdPos[thrdID]] < val)
+				{
+					dirToVal[thrdID + 1] = right;
 				}
-				else {
-					c[proc_id + 1] = left;
+				else
+				{
+					dirToVal[thrdID + 1] = left;
 				}
 			}
 
@@ -74,16 +88,20 @@ int parallelSearch(std::vector<int> arr, int val) {
 				index = temp_index;
 			}
 
-			if (c[proc_id] != c[proc_id + 1]) {
-				r = j[proc_id] - 1;
-				if (proc_id != 0)
-					l = j[proc_id - 1] + 1;
+			if (dirToVal[thrdID] != dirToVal[thrdID + 1])
+			{
+				r = thrdPos[thrdID] - 1;
+				if (thrdID != 0)
+				{
+					l = thrdPos[thrdID - 1] + 1;
+				}
 			}
 
 			#pragma omp single
 			{
-				if (c[p] != c[p + 1]) {
-					l = j[p - 1] + 1;
+				if (dirToVal[threadCount] != dirToVal[threadCount + 1])
+				{
+					l = thrdPos[threadCount - 1] + 1;
 				}
 			}
 		}
@@ -92,70 +110,86 @@ int parallelSearch(std::vector<int> arr, int val) {
 	return index;
 }
 
-int binarySearch(std::vector<int> arr, int val) {
-	int l = 0, r = N - 1;
-	int m;
-	while (l <= r) {
+template<typename VecType, typename VecSizeT = unsigned long long>
+VecSizeT binarySearch(const std::vector<VecType>& arr, VecType val)
+{
+	VecSizeT l = 0, r = N - 1;
+	VecSizeT m;
+	while (l <= r)
+	{
 		m = l + (r - l) / 2;
-		if (arr[m] == val) {
-			return m;
-		}
-		else if (arr[m] < val) {
+		if (arr[m] <= val)
+		{
 			l = m + 1;
 		}
-		else {
+		else
+		{
 			r = m - 1;
 		}
 	}
 
-	return -1;
+	return r;
 }
+
+template<typename TimeT = std::chrono::milliseconds>
+struct measure
+{
+	template<typename F, typename RetType, typename ...Args>
+	static typename TimeT::rep execution(F&& func, RetType& retVal, Args&&... args)
+	{
+		auto start = std::chrono::steady_clock::now();
+		retVal = std::forward<decltype(func)>(func)(std::forward<Args>(args)...);
+		auto duration = std::chrono::duration_cast<TimeT>
+			(std::chrono::steady_clock::now() - start);
+		return duration.count();
+	}
+};
 
 int main(int argc, char *argv[])
 {
-	std::vector<int> arr(N);
-	for (int i = 0; i < N; i++) {
+	using VecType = unsigned long long;
+	using VecSizeT = unsigned long long;
+	std::vector<VecType> arr(N);
+	for (VecSizeT i = 0; i < N; i++)
+	{
 		arr[i] = i;
 	}
 
-	int b_index, p_index;
-	int test_value;
+	VecSizeT b_index, p_index;
+	VecType test_value;
 
-	srand((unsigned int)time(NULL));
+	srand(42);
 
-	for (int i = 0; i < N; i++) {
-		//int rand_value = rand() % N;
+	for (VecSizeT i = 0; i < N; i++)
+	{
+		VecType rand_value = rand() % RAND_MAX;
 
-		test_value = i;
-		printf("searching for: %d\n", test_value);
+		test_value = rand_value;
+		printf("searching for: %lld\n", test_value);
 
 		// binary search (just for reference)
 		{
-			double start = omp_get_wtime();
-			int index = binarySearch(arr, test_value);
-			double end = omp_get_wtime();
-			printf("binarySearch time taken: %.9lf\n", end - start);
-			printf("found at: %d\n", index);
-
-			b_index = index;
+			auto duration = measure<std::chrono::nanoseconds>::execution(
+				binarySearch<VecType, VecSizeT>, b_index, arr, test_value);
+			printf("binarySearch time taken: %lld ns\n", duration);
+			printf("found at pos: %lld\n", b_index);
 		}
 
 		// parallel search
 		{
-			omp_set_num_threads(2);
-
-			double start = omp_get_wtime();
-			int index = parallelSearch(arr, test_value);
-			double end = omp_get_wtime();
-			printf("parallelSearch time taken: %.9lf\n", end - start);
-			printf("found at: %d\n", index);
-		
-			p_index = index;
+			//omp_set_num_threads(2);
+			auto duration = measure<std::chrono::nanoseconds>::execution(
+				parallelSearch<VecType, VecSizeT>, p_index, arr, test_value);
+			printf("parallelSearch time taken: %lld ns\n", duration);
+			printf("found at pos: %lld\n", p_index);
 		}
 
-		if (b_index != p_index) {
+		if (b_index != p_index)
+		{
 			break;
 		}
+
+		printf("\n");
 
 	//	system("cls");
 	}
